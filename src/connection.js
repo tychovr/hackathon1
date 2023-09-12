90fd4b8ac6ddec3996c25b811bc0c50c3c5993b2
tychovr <68756557+tychovr@users.noreply.github.com>
2023-09-12 11:43:08 +0200

    add database connection

---
+++ src/connection.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYiAYUOFut3-WIqltsIBerRXSYaXru9k0",
+  authDomain: "hackathon1-29988.firebaseapp.com",
+  projectId: "hackathon1-29988",
+  storageBucket: "hackathon1-29988.appspot.com",
+  messagingSenderId: "218849774724",
+  appId: "1:218849774724:web:57a6a8a0e0b73ea83e46d1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
