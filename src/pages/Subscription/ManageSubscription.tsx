f8def6db71c80f8da76abadcd29a23b1ae648e7e
tychovr <68756557+tychovr@users.noreply.github.com>
2023-09-13 12:05:38 +0200

    refactor: switch to material ui

--- src/pages/Subscription/ManageSubscription.tsx
+++ src/pages/Subscription/ManageSubscription.tsx
@@ -1,15 +1,16 @@
-import { useState, FormEvent } from "react";
-import { Form, Button, Container, Alert } from "react-bootstrap";
+import React, { useState, FormEvent } from "react";
+import { Container, Typography, TextField, Button, Snackbar } from "@mui/material";
 import db from "../../connection"; // Make sure to import the Firestore instance correctly
 
 const ManageSubscription = () => {
   const [DateEnd, setDateEnd] = useState("");
   const [DateStart, setDateStart] = useState("");
-  const [Price, setPrice] = useState("Basis");
+  const [Price, setPrice] = useState(0);
   const [Type, setType] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [isSuccess, setIsSuccess] = useState(false);
   const [error, setError] = useState("");
+  const [openSnackbar, setOpenSnackbar] = useState(false);
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -24,6 +25,7 @@ const ManageSubscription = () => {
       });
 
       setIsSuccess(true);
+      setOpenSnackbar(true);
     } catch (error) {
       console.error("Fout bij het aanvragen van een abonnement: ", error);
       setError("Fout bij het aanvragen van een abonnement. Probeer het later opnieuw.");
@@ -32,64 +34,78 @@ const ManageSubscription = () => {
     }
   };
 
+  const handleCloseSnackbar = () => {
+    setOpenSnackbar(false);
+  };
+
   return (
-    <Container>
-      <h1>Abonnement aanvragen</h1>
-      <Form onSubmit={(e) => handleSubmit(e)}>
-        <Form.Group controlId="naam">
-          <Form.Label>Naam</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Voer uw naam in"
-            value={DateEnd}
-            onChange={(e) => setDateEnd(e.target.value)}
-          />
-        </Form.Group>
+    <Container maxWidth="sm" sx={{ mt: 5 }}>
+      <Typography variant="h4" gutterBottom>
+        Abonnement aanvragen
+      </Typography>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <TextField
+          fullWidth
+          label="DateStart"
+          variant="outlined"
+          margin="normal"
+          type="text"
+          placeholder="Voer uw naam in"
+          value={DateEnd}
+          onChange={(e) => setDateEnd(e.target.value)}
+        />
+        <DatePicker label="Basic date picker" />
 
-        <Form.Group controlId="email">
-          <Form.Label>E-mailadres</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Voer uw e-mailadres in"
-            value={DateStart}
-            onChange={(e) => setDateStart(e.target.value)}
-          />
-        </Form.Group>
+        <TextField
+          fullWidth
+          label="E-mailadres"
+          variant="outlined"
+          margin="normal"
+          type="email"
+          placeholder="Voer uw e-mailadres in"
+          value={DateStart}
+          onChange={(e) => setDateStart(e.target.value)}
+        />
 
-        <Form.Group controlId="abonnementType">
-          <Form.Label>Prijs</Form.Label>
-          <Form.Control
-            as="text"
-            value={Price}
-            onChange={(e) => setPrice(e.target.value)}
-            disabled
-          >
-          </Form.Control>
-        </Form.Group>
+        <TextField
+          fullWidth
+          label="Prijs"
+          variant="outlined"
+          margin="normal"
+          type="text"
+          value={Price}
+          disabled
+        />
 
-        <Form.Group controlId="abonnementType">
-          <Form.Label>Abonnementstype</Form.Label>
-          <Form.Control
-            as="select"
-            value={Type}
-            onChange={(e) => setType(e.target.value)}
-          >
-            <option value="Basis">Basis</option>
-            <option value="Premium">Premium</option>
-            <option value="Deluxe">Deluxe</option>
-          </Form.Control>
-        </Form.Group>
+        <TextField
+          fullWidth
+          select
+          label="Abonnementstype"
+          variant="outlined"
+          margin="normal"
+          value={Type}
+          onChange={(e) => setType(e.target.value)}
+        >
+          <option value="Basis">Basis</option>
+          <option value="Premium">Premium</option>
+          <option value="Deluxe">Deluxe</option>
+        </TextField>
 
-        <Button variant="primary" type="submit">
+        <Button variant="contained" color="primary" type="submit" sx={{ mt: 3 }}>
           Aanmelden
         </Button>
-      </Form>
+      </form>
 
-      {isLoading && <div className="loading">Bezig met verwerken...</div>}
+      {isLoading && <Typography variant="body1" sx={{ mt: 2 }}>Bezig met verwerken...</Typography>}
       {isSuccess && (
-        <Alert variant="success">Abonnement is aangemaakt.</Alert>
+        <Snackbar
+          open={openSnackbar}
+          autoHideDuration={6000}
+          onClose={handleCloseSnackbar}
+          message="Abonnement is aangemaakt."
+        />
       )}
-      {error && <Alert variant="danger">{error}</Alert>}
+      {error && <Typography variant="body1" color="error" sx={{ mt: 2 }}>{error}</Typography>}
     </Container>
   );
 };
